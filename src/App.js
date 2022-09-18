a707e58ee9443b442741908fe9299f00fc189c41
Aaron Everette <aeverette0120@gmail.com>
2022-09-18 14:35:22 -0400

    made changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+import TopBar from './Components/TopBar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <TopBar />
     </div>
   );
 }
---
+++ src/Components/StoreListing.js
@@ -0,0 +1,15 @@
+
+
+<h2>Browse stores in Houston</h2>
+<Container style={{marginTop: "40px", textAlign: "center" }}>
+    <Row style={{ borderBottom: "1px solid #E8E9B", display: "flex", justifyContext: "space-evenly" }} >
+    
+<Row>
+    <Col xs=".5" >
+    <Image src="[IMG SRC]" roundedCircle />
+    </Col> 
+    <Col>
+    <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+    </Col>
+</Row>
+</Container>
